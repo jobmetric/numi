3d6e928a150a7ede55e3090d032309a49a3b00ab
majidmohammadian <majeedmohammadian@gmail.com>
2024-10-16 20:22:41 +0330

    added language key

---
+++ lang/en/base.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Numi Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Numi for
+    | various messages that we need to display to the user.
+    |
+    */
+
+    'description' => 'An intuitive accounting management panel designed to simplify financial tracking and reporting, empowering businesses to manage their finances with precision and ease.',
+
+    'sections' => [
+        'info' => [
+            'name' => 'Information',
+            'title' => 'Information Management',
+        ],
+        'accounting' => [
+            'name' => 'Accounting',
+            'title' => 'Accounting',
+        ],
+        'system' => [
+            'name' => 'System',
+            'title' => 'System Management',
+        ],
+        'report' => [
+            'name' => 'Reports',
+            'title' => 'Management Reports',
+        ],
+    ],
+
+];
